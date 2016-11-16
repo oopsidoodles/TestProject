7e44c7d9cf1610b6e74a30dff39171161bdd6a59
oopsidoodles <vilimilner@gmail.com>
2016-11-16 16:31:17 -0500

    test3

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my project");
-		System.out.println("more changes");
+		System.out.println("more changes"); //test
 	}
 
 }
